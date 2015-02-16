bd76f8590e5b7ff3d75b6cc8e46ab39deb2f6147
TJ Maynes <tjmaynes@outlook.com>
2015-02-16 15:59:04 -0500

    woohoo! done with task1

--- CannyBot/CannyBot.cpp
+++ CannyBot/CannyBot.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <iomanip> 
+#include <iomanip>
+#include <math.h>
+//#include <alcommon/alproxy.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -14,19 +16,20 @@
 #define UPPER_ARM_LENGTH 105
 #define SHOULDER_OFFSET_Y 98
 #define SHOULDER_OFFSET_Z 100
+#define LOWER_ARM_LENGTH 55.95
 
 const int rows = 4;
 const int columns = 4;
 
 // function headers
-static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0);
+//static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0);
 double** transformationMatrix(std::string name, double** matrix, int rows, int columns, const int a, const double alpha, const int distance, double theta);
-double** multiplyMatrices(double** RShoulderPitch, double** RShoulderRoll, double** RElbowYaw, double** RElbowRoll);
+double** multiplyMatrices(double** RShoulderPitch, double** RShoulderRoll, double** RElbowYaw, double** RElbowRoll, double** RWristRoll);
 void prettyPrint(std::string name, double** matrix);
 std::string roboShapeVision();
 
-
 // helper functions
+/*
 static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0){
 	double dx1 = pt1.x - pt0.x;
 	double dy1 = pt1.y - pt0.y;
@@ -34,18 +37,19 @@ static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0){
 	double dy2 = pt2.y - pt0.y;
 	return (dx1*dx2 + dy1*dy2) / sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
+*/
 void prettyPrint(std::string name, double** matrix){
 	std::cout << "\nThis is matrix = " << name << ".\n" << std::endl;
 	for (int i = 0; i < rows; i++){
 		for (int j = 0; j < columns; j++) {
 			if (j == 3){
 				std::cout << std::setw(6) << std::setiosflags(std::ios::fixed) << std::setprecision(3) << matrix[i][j] << "\n";
-			} else {
+			}
+			else {
 				std::cout << std::setw(6) << std::setiosflags(std::ios::fixed) << std::setprecision(3) << matrix[i][j] << " ";
 			}
 		}
 	}
-	std::cout << " " << std::endl;
 }
 
 // transformation matrix
@@ -58,49 +62,49 @@ double** transformationMatrix(std::string name_of_matrix, double** matrix, int r
 		for (int j = 0; j < columns; j++)
 		{
 			if (i == 0 && j == 0){
-				temp = std::cos(theta);
+				temp = std::cos(theta*PI / 180);
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
 			}
 			if (i == 0 && j == 1){
-				temp = (-(std::sin(theta)) * std::cos(alpha));
+				temp = (-(std::sin(theta*PI / 180)) * std::cos(alpha*PI / 180));
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
 			}
 			if (i == 0 && j == 2){
-				temp = (std::sin(theta) * std::sin(alpha));
+				temp = (std::sin(theta*PI / 180) * std::sin(alpha*PI / 180));
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
 			}
 			if (i == 0 && j == 3){
-				temp = (a * std::cos(theta));
+				temp = (a * std::cos(theta*PI / 180));
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
 			}
 			if (i == 1 && j == 0){
-				temp = (std::sin(theta));
+				temp = (std::sin(theta*PI / 180));
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
 			}
 			if (i == 1 && j == 1){
-				temp = (std::cos(theta) * std::cos(alpha));
+				temp = (std::cos(theta*PI / 180) * std::cos(alpha*PI / 180));
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
 			}
 			if (i == 1 && j == 2){
-				temp = (-(std::cos(theta)) * std::sin(alpha));
+				temp = (-(std::cos(theta*PI / 180)) * std::sin(alpha*PI / 180));
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
 			}
 			if (i == 1 && j == 3){
-				temp = (a * std::sin(theta));
+				temp = (a * std::sin(theta*PI / 180));
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
@@ -112,13 +116,13 @@ double** transformationMatrix(std::string name_of_matrix, double** matrix, int r
 				matrix[i][j] = temp;
 			}
 			if (i == 2 && j == 1){
-				temp = std::sin(alpha);
+				temp = std::sin(alpha*PI / 180);
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
 			}
 			if (i == 2 && j == 2){
-				temp = std::cos(alpha);
+				temp = std::cos(alpha*PI / 180);
 				if (temp == -0.0)
 					temp = 0.0;
 				matrix[i][j] = temp;
@@ -162,7 +166,7 @@ double** transformationMatrix(std::string name_of_matrix, double** matrix, int r
 	return matrix;
 }
 // multiply matrices
-double** multiplyMatrices(double** RShoulderPitch, double** RShoulderRoll, double** RElbowYaw, double** RElbowRoll) {
+double** multiplyMatrices(double** RShoulderPitch, double** RShoulderRoll, double** RElbowYaw, double** RElbowRoll, double** RWristRoll) {
 	double temp = 0.0;
 	// create matrix
 	double** product = new double*[columns];
@@ -213,9 +217,23 @@ double** multiplyMatrices(double** RShoulderPitch, double** RShoulderRoll, doubl
 			}
 		}
 	}
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			for (int inner = 0; inner < 4; inner++)
+			{
+				temp *= RWristRoll[inner][j];
+				if (temp == -0.0)
+					temp = 0.0;
+				product[i][j] = temp;
+			}
+		}
+	}
 	return product;
 }
 
+/*
 std::string roboShapeVision() {
 	std::cout << "Setting up NAO Robot camera!\n" << std::endl;
 	std::string shape = "";
@@ -230,7 +248,7 @@ std::string roboShapeVision() {
 
 	std::vector<std::vector<cv::Point>> contours;
 	std::vector<cv::Point> approx;
-	cv::Mat image, grayImage, blur, canny;
+	cv::Mat image, grayImage, blur, canny, dst;
 
 	int lowThreshold;
 	int const max_lowThreshold = 100;
@@ -240,10 +258,7 @@ std::string roboShapeVision() {
 	cv::waitKey(3000);
 	while (cvWaitKey(30) != 'q') {
 		camera >> image;
-		if (true)
-		{
-			cv::imshow("Streaming", image);
-
+		if (true){
 			// perform canny edge detection algorithm
 			cv::cvtColor(image, grayImage, CV_RGB2GRAY);
 
@@ -258,7 +273,7 @@ std::string roboShapeVision() {
 			// Find contours
 			cv::findContours(canny.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-			image.copyTo(canny);
+			image.copyTo(dst);
 
 			for (int i = 0; i < contours.size(); i++)
 			{
@@ -267,9 +282,10 @@ std::string roboShapeVision() {
 				cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
 
 				// Skip small or non-convex objects
-				if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx))
+				if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx)){
 					std::cout << "hello" << std::endl;
-				continue;
+					continue;
+				}
 
 				if (approx.size() == 3)
 				{
@@ -316,25 +332,30 @@ std::string roboShapeVision() {
 					int radius = r.width / 2;
 
 					if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 &&
-						std::abs(1 - (area / (CV_PI * (radius*radius)))) <= 0.2){
+						std::abs(1 - (area / (CV_PI * (radius*radius)))) <= 0.2)
+					{
 						shape = "Circle";
 						break;
 					}
 				}
 			}
+			cv::imshow("dst", dst);
+		}
+		else {
+			break;
 		}
 	}
 	return shape;
 }
-
+*/
 int main() {
-	std::cout << "Welcome to the CannyBot program!" << std::endl;
+	std::cout << "Welcome to the CannyBot program!\n" << std::endl;
 	std::string shape = "";
 
 	// return string value containing shape found on workspace.
 	//shape = roboShapeVision();
 
-	std::cout << "The shape found on the workspace was a " << shape << "." << std::endl;
+	//std::cout << "The shape found on the workspace was a " << shape << "." << std::endl;
 
 	// initialize 4x4 matrices
 	double** RShoulderPitch = new double*[columns];
@@ -353,13 +374,17 @@ int main() {
 	for (int i = 0; i < rows; i++){
 		RElbowRoll[i] = new double[rows];
 	}
+	double** RWristRoll = new double*[columns];
+	for (int i = 0; i < rows; i++){
+		RWristRoll[i] = new double[rows];
+	}
 	double** base_to_start = new double*[columns];
 	for (int i = 0; i < rows; i++){
 		base_to_start[i] = new double[rows];
 	}
 
 	// read input file of theta values
-	double theta1 = 0.0, theta2 = 0.0, theta3 = 0.0, theta4 = 0.0;
+	double theta1 = 0.0, theta2 = 0.0, theta3 = 0.0, theta4 = 0.0, theta5 = 0.0;
 	std::string filename = "input.txt";
 	std::ifstream fs;
 	fs.open(filename.c_str());
@@ -370,10 +395,13 @@ int main() {
 	fs >> theta2;
 	fs >> theta3;
 	fs >> theta4;
+	fs >> theta5;
 
 	// stop reading from input file
 	std::cout << "Finished processing file." << std::endl;
 	fs.close();
+
+	std::cout << "\nThetas are " << theta1 << ", " << theta2 << ", " << theta3 << ", " << theta4 << ", " << theta5 << std::endl;
 
 	// create matrices
 	// pass theta value per row to specified matrix for Right Arm of NAO Robot
@@ -381,9 +409,10 @@ int main() {
 	RShoulderRoll = transformationMatrix("RShoulderRoll", RShoulderRoll, rows, columns, 0, (PI / 2.0), 0, theta2 + (PI / 2.0));
 	RElbowYaw = transformationMatrix("RElbowYaw", RElbowYaw, rows, columns, -ELBOW_OFFSET_Y, (PI / 2.0), UPPER_ARM_LENGTH, theta3);
 	RElbowRoll = transformationMatrix("RElbowRoll", RElbowRoll, rows, columns, 0, -(PI / 2.0), 0, theta4);
+	RWristRoll = transformationMatrix("RWristYRoll", RWristRoll, rows, columns, LOWER_ARM_LENGTH, (PI / 2.0), 0, theta5);
 
 	// mulitple matrices together for matrix
-	base_to_start = multiplyMatrices(RShoulderPitch, RShoulderRoll, RElbowYaw, RElbowRoll);
+	base_to_start = multiplyMatrices(RShoulderPitch, RShoulderRoll, RElbowYaw, RElbowRoll, RWristRoll);
 
 	// print resulting base_to_start matrix
 	prettyPrint("base_to_start", base_to_start);
@@ -392,5 +421,6 @@ int main() {
 
 
 	// program exit
+	std::cout << "\nend of program" << std::endl;
 	return 0;
 }
