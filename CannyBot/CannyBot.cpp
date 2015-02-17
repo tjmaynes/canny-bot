61ed7d2901d39e1cc8da27bd57976cc9551c658d
TJ Maynes <tjmaynes@outlook.com>
2015-02-16 19:32:21 -0500

    detected lines using canny edge detection!

--- CannyBot/CannyBot.cpp
+++ CannyBot/CannyBot.cpp
@@ -22,14 +22,29 @@ const int rows = 4;
 const int columns = 4;
 
 // function headers
-//static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0);
+static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0);
 double** transformationMatrix(std::string name, double** matrix, int rows, int columns, const int a, const double alpha, const int distance, double theta);
 double** multiplyMatrices(double** RShoulderPitch, double** RShoulderRoll, double** RElbowYaw, double** RElbowRoll, double** RWristRoll);
 void prettyPrint(std::string name, double** matrix);
 std::string roboShapeVision();
 
+void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
+{
+	int fontface = cv::FONT_HERSHEY_SIMPLEX;
+	double scale = 0.4;
+	int thickness = 1;
+	int baseline = 0;
+
+	cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
+	cv::Rect r = cv::boundingRect(contour);
+
+	cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
+	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255, 255, 255), CV_FILLED);
+	cv::putText(im, label, pt, fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
+}
+
+
 // helper functions
-/*
 static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0){
 	double dx1 = pt1.x - pt0.x;
 	double dy1 = pt1.y - pt0.y;
@@ -37,7 +52,7 @@ static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0){
 	double dy2 = pt2.y - pt0.y;
 	return (dx1*dx2 + dy1*dy2) / sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
-*/
+
 void prettyPrint(std::string name, double** matrix){
 	std::cout << "\nThis is matrix = " << name << ".\n" << std::endl;
 	for (int i = 0; i < rows; i++){
@@ -169,75 +184,90 @@ double** transformationMatrix(std::string name_of_matrix, double** matrix, int r
 double** multiplyMatrices(double** RShoulderPitch, double** RShoulderRoll, double** RElbowYaw, double** RElbowRoll, double** RWristRoll) {
 	double temp = 0.0;
 	// create matrix
-	double** product = new double*[columns];
+	double** product1 = new double*[columns];
 	for (int i = 0; i < rows; i++){
-		product[i] = new double[rows];
+		product1[i] = new double[rows];
 	}
-	double** base = new double*[columns];
+	double** product2 = new double*[columns];
 	for (int i = 0; i < rows; i++){
-		product[i] = new double[rows];
+		product2[i] = new double[rows];
+	}
+	double** product3 = new double*[columns];
+	for (int i = 0; i < rows; i++){
+		product3[i] = new double[rows];
+	}
+
+	double** product4 = new double*[columns];
+	for (int i = 0; i < rows; i++){
+		product4[i] = new double[rows];
 	}
 
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < columns; j++)
 		{
+			product1[i][j] = 0;
 			for (int inner = 0; inner < 4; inner++)
 			{
-				temp = RShoulderPitch[i][inner] * RShoulderRoll[inner][j];
-				if (temp == -0.0)
-					temp = 0.0;
-				product[i][j] = temp;
+				product1[i][j] += RShoulderPitch[i][inner] * RShoulderRoll[inner][j];
 			}
 		}
 	}
+
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < columns; j++)
 		{
+			product2[i][j] = 0;
 			for (int inner = 0; inner < 4; inner++)
 			{
-				temp *= RElbowYaw[inner][j];
-				if (temp == -0.0)
-					temp = 0.0;
-				product[i][j] = temp;
+				product2[i][j] += product1[i][inner] * RElbowRoll[inner][j];
 			}
 		}
 	}
+
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < columns; j++)
 		{
+			product3[i][j] = 0;
 			for (int inner = 0; inner < 4; inner++)
 			{
-				temp *= RElbowRoll[inner][j];
-				if (temp == -0.0)
-					temp = 0.0;
-				product[i][j] = temp;
+				product3[i][j] += product2[i][inner] * RElbowYaw[inner][j];
 			}
 		}
 	}
+
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < columns; j++)
 		{
+			product4[i][j] = 0;
 			for (int inner = 0; inner < 4; inner++)
 			{
-				temp *= RWristRoll[inner][j];
-				if (temp == -0.0)
-					temp = 0.0;
-				product[i][j] = temp;
+				product4[i][j] += product3[i][j] * RWristRoll[inner][j];
 			}
 		}
 	}
-	return product;
+	return product4;
 }
 
-/*
+
+// captures object very quickly...
+// need to make sure camera is already looking at workspace!
+//
 std::string roboShapeVision() {
 	std::cout << "Setting up NAO Robot camera!\n" << std::endl;
 	std::string shape = "";
 
+	bool breakout = false;
+	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Point> approx;
+	cv::Mat image, grayImage, blur, canny, dst;
+	int lowThreshold;
+	int const max_lowThreshold = 100;
+	int ratio = 3;
+	int kernel_size = 3;
 	cv::VideoCapture camera;
 	int camOpen = camera.open(CV_CAP_ANY);
 
@@ -246,17 +276,11 @@ std::string roboShapeVision() {
 	}
 	cv::namedWindow("CameraCapture", CV_WINDOW_AUTOSIZE);
 
-	std::vector<std::vector<cv::Point>> contours;
-	std::vector<cv::Point> approx;
-	cv::Mat image, grayImage, blur, canny, dst;
-
-	int lowThreshold;
-	int const max_lowThreshold = 100;
-	int ratio = 3;
-	int kernel_size = 3;
-
+	// wait 3 seconds for robo camera to be ready
 	cv::waitKey(3000);
-	while (cvWaitKey(30) != 'q') {
+
+	// breakout of loop when shape is found
+	while (!breakout) {
 		camera >> image;
 		if (true){
 			// perform canny edge detection algorithm
@@ -269,93 +293,101 @@ std::string roboShapeVision() {
 			// show canny window called robovision
 			cv::imshow("RoboVision", canny);
 
-			// break out of contour drawing
 			// Find contours
-			cv::findContours(canny.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+			cv::findContours(canny.clone(), contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 
 			image.copyTo(dst);
 
-			for (int i = 0; i < contours.size(); i++)
-			{
-				// Approximate contour with accuracy proportional
-				// to the contour perimeter
-				cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
-
-				// Skip small or non-convex objects
-				if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx)){
-					std::cout << "hello" << std::endl;
-					continue;
-				}
-
-				if (approx.size() == 3)
+				for (int i = 0; i < contours.size(); i++)
 				{
-					shape = "Triangle"; // Triangles
-					break;
-				}
-				else if (approx.size() >= 4 && approx.size() <= 6)
-				{
-					// Number of vertices of polygonal curve
-					int vtc = approx.size();
+					// Approximate contour with accuracy proportional
+					// to the contour perimeter
+					cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
 
-					// Get the cosines of all corners
-					std::vector<double> cos;
-					for (int j = 2; j < vtc + 1; j++)
-						cos.push_back(angle(approx[j%vtc], approx[j - 2], approx[j - 1]));
+					// Skip small or non-convex objects
 
-					// Sort ascending the cosine values
-					std::sort(cos.begin(), cos.end());
-
-					// Get the lowest and the highest cosine
-					double mincos = cos.front();
-					double maxcos = cos.back();
-
-					// Use the degrees obtained above and the number of vertices
-					// to determine the shape of the contour
-					if (vtc == 4){
-						shape = "Rectangle";
-						break;
+					if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx)){
+						continue;
 					}
-					else if (vtc == 5){
-						shape = "Pentagon";
-						break;
-					}
-					else if (vtc == 6){
-						shape = "Hexagon";
-						break;
-					}
-				}
-				else
-				{
-					// Detect and label circles
-					double area = cv::contourArea(contours[i]);
-					cv::Rect r = cv::boundingRect(contours[i]);
-					int radius = r.width / 2;
 
-					if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 &&
-						std::abs(1 - (area / (CV_PI * (radius*radius)))) <= 0.2)
+					if (approx.size() == 3)
 					{
-						shape = "Circle";
+						setLabel(dst, "TRI", contours[i]);
+						shape = "Triangle"; // Triangles
+						breakout = true;
+					}
+					else if (approx.size() >= 4 && approx.size() <= 6)
+					{
+						// Number of vertices of polygonal curve
+						int vtc = approx.size();
+
+						// Get the cosines of all corners
+						std::vector<double> cos;
+						for (int j = 2; j < vtc + 1; j++)
+							cos.push_back(angle(approx[j%vtc], approx[j - 2], approx[j - 1]));
+
+						// Sort ascending the cosine values
+						std::sort(cos.begin(), cos.end());
+
+						// Get the lowest and the highest cosine
+						double mincos = cos.front();
+						double maxcos = cos.back();
+
+						// Use the degrees obtained above and the number of vertices
+						// to determine the shape of the contour
+						if (vtc == 4){
+							setLabel(dst, "RECT", contours[i]);
+							shape = "Rectangle";
+							breakout = true;
+						}
+						else if (vtc == 5){
+							setLabel(dst, "PEN", contours[i]);
+							shape = "Pentagon";
+							breakout = true;
+						}
+						else if (vtc == 6){
+							setLabel(dst, "HEX", contours[i]);
+							shape = "Hexagon";
+							breakout = true;
+						}
+					}
+					else
+					{
+						// Detect and label circles
+						double area = cv::contourArea(contours[i]);
+						cv::Rect r = cv::boundingRect(contours[i]);
+						int radius = r.width / 2;
+
+						if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 &&
+							std::abs(1 - (area / (CV_PI * (radius*radius)))) <= 0.2)
+						{
+							setLabel(dst, "CIR", contours[i]);
+							shape = "Circle";
+							breakout = true;
+						}
+					}
+					if (breakout){
 						break;
 					}
 				}
-			}
-			cv::imshow("dst", dst);
+				cv::imshow("dst", dst);
 		}
 		else {
 			break;
 		}
 	}
+	cvDestroyAllWindows();
 	return shape;
 }
-*/
+
 int main() {
 	std::cout << "Welcome to the CannyBot program!\n" << std::endl;
 	std::string shape = "";
 
 	// return string value containing shape found on workspace.
-	//shape = roboShapeVision();
+	shape = roboShapeVision();
 
-	//std::cout << "The shape found on the workspace was a " << shape << "." << std::endl;
+	std::cout << "The shape found on the workspace was a " << shape << ".\n" << std::endl;
 
 	// initialize 4x4 matrices
 	double** RShoulderPitch = new double*[columns];
@@ -417,7 +449,7 @@ int main() {
 	// print resulting base_to_start matrix
 	prettyPrint("base_to_start", base_to_start);
 
-	// pass shape value to 
+	// pass shape value to somewhere
 
 
 	// program exit
