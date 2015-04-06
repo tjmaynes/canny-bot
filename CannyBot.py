17b0292767f1770823ee6c4abdf09f1cdd789294
TJ Maynes <tjmaynes@outlook.com>
2015-04-06 17:04:00 -0400

    need to finish grid!

--- CannyBot.py
+++ CannyBot.py
@@ -149,119 +149,132 @@ def robo_vision():
   if c == 27:
     exit(0)
 
+def create_grid(start):
+  rows = 7
+  columns = 7
+  grid = [[0 for x in range(rows)] for x in range(columns)]
+
+  # traverse through matrix and add theta values based on measurements of space in "invisible" grid
+  for i in range(rows):
+    for j in range(columns:
+      if i == 0 and j == 0:
+        grid[i][j] = start
+      elif i == 0 and j == 1:
+        grid[i][j] = [1,2,1,2]
+      else:
+        break
+
+  return grid
+
 def robo_motion(shape_name):
   voice.say("I will draw a " + shape);
 
     print "\nNAO Robot will draw this shape: " + shape + "."
 
-    # initialize matrices
-    RShoulderPitch = [[0 for x in range(4)] for x in range(4)]
-    RShoulderRoll = [[0 for x in range(4)] for x in range(4)]
-    RElbowYaw = [[0 for x in range(4)] for x in range(4)]
-    RElbowRoll = [[0 for x in range(4)] for x in range(4)]
-    RWristRoll = [[0 for x in range(4)] for x in range(4)]
-    base_to_start = [[0 for x in range(4)] for x in range(4)]
-
     stiffness_on(motionProxy)
+
+    start = [0,10,0,-1]
+
+    grid = create_grid(start)
 
     # Send NAO to Pose Init
     postureProxy.goToPosture("StandInit", 0.5)
 
     effector   = "RArm"
-    space      = motion.FRAME_BODY
+    space      = motion.FRAME_ROBOT
     axisMask   = almath.AXIS_MASK_VEL    # just control position
     isAbsolute = False
 
    # Since we are in relative, the current position is zero
-   currentPos = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+   current_pos = transformation_matrices(grid[0][0])
 
    if shape_name is "line":
-     path = [transformation_matrices(0,0,10,0), currentPos]
-     times = [2.0, 10.0]
+     path = [current_pos, transformation_matrices(grid[5,0])]
+     times = [2.0, 6.0]
    elif shape_name is "square":
-     break
+     path = [current_pos, transformation_matrices(grid[5,0]), transformation_matrices(grid[5,5]), transformation_matrics(grid[0,5]), current_pos]
+     times = [2.0, 4.0, 4.0, 4.0, 2.0]
    elif shape_name is "triangle":
-     break
-   elif shape_name is "circle":
-     break
+     path = [current_pos, transformation_matrics(grid[5,5]), transformation_matrics(grid[0,5]), current_pos]
+     times = [2.0, 4.0, 4.0, 2.0]
    else:
      print shape_name + " was not programmed to be drawn."
 
-   motionProxy.positionInterpolation(effector, space, path, axisMask, times, isAbsolute)
+   motionProxy.transferInterpolation(effector, space, path, axisMask, times, isAbsolute)
 
 def transformation_matrix(name_of_matrix, matrix, rows, columns, a, alpha, distance, theta):
   temp = 0.0
-         for i in range(rows):
-           for j in range(columns):
-             if i == 0 and j == 0:
-               temp = math.cos(theta*math.pi/180.0)
+  for i in range(rows):
+    for j in range(columns):
+      if i == 0 and j == 0:
+        temp = math.cos(theta*math.pi/180.0)
+        if temp == -0:
+         temp = 0.0
+        matrix[i][j] = round(temp)
+      elif i == 0 and j == 1:
+         temp = (-(math.sin(theta*math.pi/180.0))*math.cos(alpha*math.pi/180.0))
          if temp == -0:
            temp = 0.0
          matrix[i][j] = round(temp)
-         if i == 0 and j == 1:
-           temp = (-(math.sin(theta*math.pi/180.0))*math.cos(alpha*math.pi/180.0))
+      elif i == 0 and j == 2:
+         temp = (math.sin(theta*math.pi/ 180.0) * math.sin(alpha*math.pi/ 180.0))
          if temp == -0:
            temp = 0.0
          matrix[i][j] = round(temp)
-         if i == 0 and j == 2:
-           temp = (math.sin(theta*math.pi/ 180.0) * math.sin(alpha*math.pi/ 180.0))
+      elif i == 0 and j == 3:
+         temp = (a * math.cos(theta*math.pi/180.0))
          if temp == -0:
            temp = 0.0
          matrix[i][j] = round(temp)
-         if i == 0 and j == 3:
-           temp = (a * math.cos(theta*math.pi/180.0))
+      elif i == 1 and j == 0:
+         temp = math.sin(theta*math.pi/180.0)
          if temp == -0:
            temp = 0.0
          matrix[i][j] = round(temp)
-         if i == 1 and j == 0:
-           temp = math.sin(theta*math.pi/180.0)
+      elif i == 1 and j == 1:
+         temp = (math.cos(theta*math.pi/180.0)*math.cos(alpha*math.pi/180.0))
          if temp == -0:
            temp = 0.0
          matrix[i][j] = round(temp)
-         if i == 1 and j == 1:
-           temp = (math.cos(theta*math.pi/180.0)*math.cos(alpha*math.pi/180.0))
-                if temp == -0:
-                  temp = 0.0
-                matrix[i][j] = round(temp)
-            if i == 1 and j == 2:
-              temp = (-(math.cos(theta*math.pi/180.0))*math.sin(alpha*math.pi/180.0))
-                if temp == -0:
-                  temp = 0.0
-                matrix[i][j] = round(temp)
-            if i == 1 and j == 3:
-              temp = a*math.sin(theta*math.pi/180.0)
-                if temp == -0:
-                  temp = 0.0
-                matrix[i][j] = round(temp)
-            if i == 2 and j == 0:
-              matrix[i][j] = 0.0
-            if i == 2 and j == 1:
-              temp = math.sin(alpha * math.pi/180.0)
-                if temp == -0:
-                  temp = 0.0
-                matrix[i][j] = round(temp)
-            if i == 2 and j == 2:
-              temp = math.cos(alpha * math.pi/180.0)
-                if temp == -0:
-                  temp = 0.0
-                matrix[i][j] = round(temp)
-            if i == 2 and j == 3:
-              temp = distance
-                if temp == -0:
-                  temp = 0.0
-                matrix[i][j] = round(temp)
-            if i == 3 and j == 0:
-              matrix[i][j] = 0.0
-            if i == 3 and j == 1:
-              matrix[i][j] = 0.0
-            if i == 3 and j == 2:
-              matrix[i][j] = 0.0
-            if i == 3 and j == 3:
-              matrix[i][j] = 1.0
-    return matrix
+      elif i == 1 and j == 2:
+         temp = (-(math.cos(theta*math.pi/180.0))*math.sin(alpha*math.pi/180.0))
+         if temp == -0:
+           temp = 0.0
+         matrix[i][j] = round(temp)
+      elif i == 1 and j == 3:
+         temp = a*math.sin(theta*math.pi/180.0)
+         if temp == -0:
+           temp = 0.0
+         matrix[i][j] = round(temp)
+      elif i == 2 and j == 0:
+         matrix[i][j] = 0.0
+      elif i == 2 and j == 1:
+         temp = math.sin(alpha * math.pi/180.0)
+         if temp == -0:
+            temp = 0.0
+         matrix[i][j] = round(temp)
+      elif i == 2 and j == 2:
+         temp = math.cos(alpha * math.pi/180.0)
+         if temp == -0:
+            temp = 0.0
+         matrix[i][j] = round(temp)
+      elif i == 2 and j == 3:
+         temp = distance
+         if temp == -0:
+            temp = 0.0
+         matrix[i][j] = round(temp)
+      elif i == 3 and j == 0:
+         matrix[i][j] = 0.0
+      elif i == 3 and j == 1:
+         matrix[i][j] = 0.0
+      elif i == 3 and j == 2:
+         matrix[i][j] = 0.0
+      elif i == 3 and j == 3:
+         matrix[i][j] = 1.0
+  return matrix
 
 def  multiply_matrices(RShoulderPitch, RShoulderRoll, RElbowYaw, RElbowRoll, RWristRoll):
-  # initialize temp matrices
+    # initialize temp matrices
     m0 = [[0 for x in range(4)] for x in range(4)]
     m1 = [[0 for x in range(4)] for x in range(4)]
     m2 = [[0 for x in range(4)] for x in range(4)]
@@ -298,21 +311,8 @@ def transformation_matrices(theta0, theta1, theta2, theta3):
     RWristRoll = [[0 for x in range(4)] for x in range(4)]
     base_to_start = [[0 for x in range(4)] for x in range(4)]
 
-    """
-    # process file
-    print("Processing files in input.txt.")
-    f = open('input.txt', 'r')
-    theta0 = f.readline()
-    theta1 = f.readline()
-    theta2 = f.readline()
-    theta3 = f.readline()
-    theta4 = f.readline()
-    f.close()
-    print "Finished processing file."
-    """
-
     # bounds checking (to prevent overheating)
-    print "(Before check): Thetas are %d, %d, %d, %d, %d" % (float(theta0), float(theta1), float(theta2), float(theta3), float(theta4))
+    #print "(Before check): Thetas are %d, %d, %d, %d, %d" % (float(theta0), float(theta1), float(theta2), float(theta3), float(theta4))
 
     if float(theta0) >= 119.5:
       theta0 = 110
@@ -335,21 +335,15 @@ def transformation_matrices(theta0, theta1, theta2, theta3):
     elif float(theta4) <= -104.5:
       theta4 = -100
 
-    print "\n(After check): Thetas are %d, %d, %d, %d, %d" % (float(theta0), float(theta1), float(theta2), float(theta3), float(theta4))
+    #print "\n(After check): Thetas are %d, %d, %d, %d, %d" % (float(theta0), float(theta1), float(theta2), float(theta3), float(theta4))
 
     # transformation matrices
     RShoulderPitch = transformation_matrix("RShoulderPitch",RShoulderPitch,rows,columns,0,-(math.pi/2.0), 0, float(theta0))
-    pretty_print("RShoulderPitch", RShoulderPitch)
     RShoulderRoll = transformation_matrix("RShoulderRoll",RShoulderRoll,rows,columns,0,math.pi/2.0, 0, float(theta1) + (math.pi/2.0))
-    pretty_print("RShoulderRoll", RShoulderRoll)
     RElbowYaw = transformation_matrix("RElbowYaw",RElbowYaw,rows,columns,-ELBOW_OFFSET_Y, (math.pi / 2.0), UPPER_ARM_LENGTH, float(theta2))
-    pretty_print("RElbowYaw",RElbowYaw)
     RElbowRoll = transformation_matrix("RElbowRoll",RElbowRoll,rows,columns,0, -(math.pi / 2.0), 0, float(theta3))
-    pretty_print("RElbowRoll",RElbowRoll)
     RWristRoll = transformation_matrix("RWristRoll",RWristRoll,rows,columns,LOWER_ARM_LENGTH, (math.pi/ 2.0), 0, float(theta4))
-    pretty_print("RWristRoll",RWristRoll)
     base_to_start = multiply_matrices(RShoulderPitch,RShoulderRoll,RElbowYaw,RElbowRoll,RWristRoll)
-    pretty_print("base_to_start", base_to_start)
 
     return base_to_start
 
